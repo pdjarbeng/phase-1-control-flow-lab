ee3ff1f9228adb9755a0922f2ded043fbc0044be
priscilla <pdjarbeng@outlook.com>
2021-10-12 20:56:04 -0400

    commit tree 68bfcb94cb0d2536a46318403a44befb9daebf29

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  if(ride <= 400) {
+    return "This one is on me!"
+} else if(ride > 2500){
+  return "No can do."
+} else {
+    return "I will gladly take your thirty bucks." 
+  
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  return city === 'NYC'?'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default: 
+      return "Bye."  
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function scuberGreetingForFeet(ride) {
+//   if(ride <= "400") {
+//     return "This one is on me!"
+// } else if(ride <= "2499") {
+//     return "I will gladly take your thirty bucks."
+// } else {
+//     return "No can do."
+//   }
+// }
